deaa7966d6623398df8f285e7489b8608c76df4b
RenanBRrs <renandossantosbrito@hotmail.com>
2021-11-24 16:39:52 -0300

    Photo_Header

---
+++ src/components/UserPhoto/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from '../../theme/colors';
+
+export const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  avatar: {
+    borderWidth: 4,
+    borderColor: COLORS.BLACK_SECONDARY,
+  },
+});
--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -4,6 +4,7 @@ export const COLORS = {
   PINK: '#FF008E',
   ORANGE: '#FF7A29',
   YELLOW: '#FFCD1E',
+  BLUE: '#00FF',
 
   BLACK_PRIMARY: '#09090A',
   BLACK_SECONDARY: '#121214',
@@ -13,4 +14,4 @@ export const COLORS = {
   GRAY_SECONDARY: '#C4C4CC',
   GRAY_TERTIARY: '#E1E1E6',
   GRAY_QUATERNARY: '#29292E',
-}
+};
